cab7f4c95222815e47757345e425e2dd00130398
Alvaro Inclan <alvaroinclan8@gmail.com>
2026-03-31 16:52:48 +0200

    Add pytest test suite with coverage for weather.utils

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+import pytest
+import pandas as pd
+from weather.utils import celsius_to_fahrenheit
+
+class TestCelsiusToFahrenheit:
+    def test_freezing_point(self):
+        assert celsius_to_fahrenheit(0) == 32.0
+
+    def test_boiling_point(self):
+        assert celsius_to_fahrenheit(100) == 212.0
+
+    def test_negative_forty_is_equal_in_both_scales(self):
+        assert celsius_to_fahrenheit(-40) == -40.0
